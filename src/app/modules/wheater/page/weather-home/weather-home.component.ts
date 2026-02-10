bfb9b6340d15b6aab0c3fe2cb308a6e2148e3948
pauloalvesm <paulo.alves7351@gmail.com>
2026-02-10 10:36:18 -0300

    feat: apply weather service usage

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherData } from '../../../../models/interfaces/WeatherData';
 
 @Component({
   selector: 'app-weather-home',
   standalone: false,
   templateUrl: './weather-home.component.html',
-  styleUrl: './weather-home.component.scss'
+  styleUrl: './weather-home.component.scss',
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'Recife';
+  weatherData!: WeatherData;
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherData(this.initialCityName);
+  }
+
+  getWeatherData(cityName: string): void {
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        response && (this.weatherData = response);
+        console.log(this.weatherData);
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
